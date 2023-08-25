996bbd9637989c2c6cb524201271a73d33954124
Javier Quinteros <javier@quinteros.eu>
2023-08-25 14:18:54 +0200

    Fix format of the HTML documentation

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['python-dateutil', 'requests'],
+    install_requires=['python-dateutil', 'requests', 'sphinx-rtd-theme'],
 
     python_requires='>=2.7',
     # List additional groups of dependencies here (e.g. development
